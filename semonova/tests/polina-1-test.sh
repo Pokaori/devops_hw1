75845cb49051842c66c666cd2dcaf197621bebb6
Pokaori <sempolina15@gmail.com>
2022-09-21 00:14:27 +0300

    cretae module semonova

---
+++ semonova/tests/polina-1-test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env roundup
+#
+#/ usage:  rerun stubbs:test -m semonova -p polina [--answers <>]
+#
+
+# Helpers
+# -------
+[[ -f ./functions.sh ]] && . ./functions.sh
+
+# The Plan
+# --------
+describe "polina"
+
+# ------------------------------
+# Replace this test. 
+it_fails_without_a_real_test() {
+    exit 1
+}
+# ------------------------------
+
